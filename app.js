e9538cb746575237670928bdca0825e7d81969c3
Pr4ct1c4s <135194261+Pr4ct1c4s@users.noreply.github.com>
2023-06-03 00:05:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Muestra un cambio'));
+app.get('/', (req, res) => res.send('Practica 3! Muestra un cambio'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
